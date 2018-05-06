7f32202d5ffeb82bed00f613804713f64b9362b4
Bradley Dettmer <11279144+redSlug@users.noreply.github.com>
2018-05-05 21:29:42 -0400

    implementing my own request parsing

--- ppm_server/src/main.rs
+++ ppm_server/src/main.rs
@@ -2,44 +2,114 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::string::String;
+use std::str;
 extern crate regex;
 use regex::Regex;
 use std::fs::File;
+use std::io;
 use std::path::Path;
+use std::io::BufReader;
+use std::collections::HashMap;
+use std::io::{Error, ErrorKind};
 
-const MAX_SIZE_IN_BYTES: usize = 65536;
 
+#[derive(PartialEq)]
+enum Method {
+    Get,
+    Post,
+}
+
+impl Method {
+    fn from_string(s: &str) -> io::Result<Method> {
+        match s {
+            "GET" => Ok(Method::Get),
+            "POST" => Ok(Method::Post),
+            &_ => Err(Error::new(ErrorKind::Other, format!("{} Unsupported Method", s))),
+        }
+    }
+}
+
+struct Request {
+    method: Method,
+    path: String,
+    headers: HashMap<String, String>,
+    body: String,
+}
+
+impl Request {
+    fn from_stream(stream: &mut TcpStream) -> io::Result<Request> {
+        let mut reader = BufReader::new(stream);
+        let (method, path) = Request::read_status(&mut reader)?;
+        let headers = Request::read_headers(&mut reader)?;
+        let body = Request::read_body(&mut reader, Request::get_content_length(&headers))?;
+
+        // returns
+        Ok(Request {
+            method,
+            path,
+            headers,
+            body,
+        })
+    }
+
+    fn get_content_length(headers: &HashMap<String, String>) -> usize{
+        let content_length = "Content-Length";
+        if !headers.contains_key(content_length) {
+            return 0;
+        }
+        return headers.get(content_length).unwrap().parse::<usize>().unwrap();
+    }
+
+    fn read_line(reader: &mut BufRead) -> io::Result<String> {
+        let mut buffer = Vec::new();
+        reader.read_until(b'\n', &mut buffer).unwrap();
+        let s = std::str::from_utf8(&buffer).unwrap();
+        return Ok(s.to_string());
+    }
+
+    fn read_status(reader: &mut BufRead) -> io::Result<(Method, String)> {
+        let data = Request::read_line(reader).unwrap();
+        let v: Vec<&str> = data.split(' ').collect();
+        return Ok((Method::from_string(v[0]).unwrap(), v[1].to_string()));
+    }
+
+    fn read_headers(reader: &mut BufRead) -> io::Result<HashMap<String, String>> {
+        let mut headers = HashMap::new();
+        loop {
+            let l = Request::read_line(reader)?;
+            let v: Vec<&str> = l.split(":").collect();
+            if v.len() < 2 {
+                // No more headers to parse
+                return Ok(headers);
+            }
+            headers.insert(v[0].trim().to_string(), v[1].trim().to_string());
+        }
+    }
+
+    fn read_body(reader: &mut BufRead, content_length: usize) -> io::Result<String> {
+        if content_length == 0 {
+            return Ok("no payload".to_string());
+        }
+        let mut buffer = vec![0u8; content_length];
+        reader.read_exact(&mut buffer).unwrap();
+        let res = str::from_utf8(&buffer).unwrap();
+        return Ok(res.to_string());
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
     }
 }
 
-
 fn get_rgb(hex_color: &str) -> Vec<u8>{
-    let r;
-    let g;
-    let b ;
-    if hex_color.chars().count() == 3 {
-        // 1 byte colors
-        r = u8::from_str_radix(&format!("{}{}", &hex_color[0..1], &hex_color[0..1]), 16);
-        g = u8::from_str_radix(&format!("{}{}", &hex_color[1..2], &hex_color[1..2]), 16);
-        b = u8::from_str_radix(&format!("{}{}", &hex_color[2..3], &hex_color[2..3]), 16);
-    } else {
-        // 2 byte colors
-        r = u8::from_str_radix(&hex_color[0..2], 16);
-        g = u8::from_str_radix(&hex_color[2..4], 16);
-        b = u8::from_str_radix(&hex_color[4..6], 16);
-    }
-    // Need it to be a vector so get append to work
     let mut ret = Vec::new();
-    ret.push(r.unwrap());
-    ret.push(g.unwrap());
-    ret.push(b.unwrap());
+    ret.push(u8::from_str_radix(&hex_color[0..2], 16).unwrap());
+    ret.push(u8::from_str_radix(&hex_color[2..4], 16).unwrap());
+    ret.push(u8::from_str_radix(&hex_color[4..6], 16).unwrap());
     return ret;
 }
 
@@ -72,27 +142,23 @@ fn get_ppm_file(input: &str, cols: i32) {
     };
 }
 
-
 fn handle_connection(mut stream: TcpStream) {
     println!("connection");
-
-    let mut buffer = [0; MAX_SIZE_IN_BYTES];
-    stream.read(&mut buffer).unwrap();
-    let payload = String::from_utf8_lossy(&buffer[..]); // from utf8 converts it to String instead of COW
-    let response;
-
-    let mut f = File::open("src/index.html").expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    if payload.contains("POST"){
+    let r = Request::from_stream(&mut stream).unwrap();
+    let mut response = format!("HTTP/1.1 {}\r\n\r\n\n{}\n", "400 BAD", "unimplemented");
+    if r.method == Method::Get {
+        let mut f = File::open("src/index.html").expect("file not found");
+        let mut contents = String::new();
+        f.read_to_string(&mut contents)
+            .expect("something went wrong reading the file");
+        response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{}\r\n", contents);
+    } else if r.method == Method::Post {
         println!("post request!");
-        println!("payload={}", &payload); // data in paload missing
+        let body = r.body;
+        println!("body={}", body);
         println!("before regex");
         let data_re = Regex::new(r"cols=(?P<cols>[0-9]+)\#(?P<data>([0-9a-zA-Z\#])+)").unwrap();
-        let cap = data_re.captures(&payload);
-
+        let cap = data_re.captures(&body);
         match cap {
             None => {
                 response = format!("HTTP/1.1 {}\r\n\r\n\n{}\n", "400 BAD", "payload error");
@@ -106,14 +172,7 @@ fn handle_connection(mut stream: TcpStream) {
                 response = format!("HTTP/1.1 {}\r\n\r\n\n{}\n", "200 OK", data);
             }
         }
-    } else if payload.contains("GET"){
-        response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n{}\r\n", contents);
-
-    } else {
-        response = format!("HTTP/1.1 {}\r\n\r\n\n{}\n", "400 BAD", "nope!");
     }
-
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
 }
